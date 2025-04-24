64bab228002bcf01c8ca2fcc0245c3dff0f881a3
AmeelMD <ameel.md@icloud.com>
2025-04-24 18:20:45 -0400

    removing to readd
    removing to readd

--- install/proxmox.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-install_tools_proxmox() {
-  echo "🚀 Installing tools on Proxmox VE..."
-  source "$(dirname "${BASH_SOURCE[0]}")/linux.sh"
-  install_tools_linux
-  apt install -y pve-manager pve-cluster || true
-}
